12a4d7be0c054797f0a1349f3f02766de306affd
YaolinTan <yaolint@nycstudents.net>
2024-11-06 17:31:56 -0500

    card inject

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -1,25 +1,32 @@
 import "../CSS/style.css";
 import { lecards } from "./product";
 import { DOMselectors } from "./dom";
-const lebrons = lecards.filter((lecard) => lecard.name === "LeBron James");
 DOMselectors.lebronButton.addEventListener("click", function (event) {
   event.preventDefault();
   const lebrons = lecards.filter((lecard) => lecard.name === "LeBron James");
   lebrons.forEach((lebron) => {
-    let player = lebrons;
+    event.preventDefault();
+    let player = lebron;
+    const cardObject = createCardObject(player);
+    injectCard(cardObject);
   });
 });
 
-function createCardObject() {
+function createCardObject(player) {
   return {
     name: player.name,
     year: player.year,
-    brand: player.brand,
+    brand: player.card_brand,
     stats: player.stats,
     bPlace: player.birthplace,
     bYear: player.birthYear,
-    school: player.school,
-    dYear: player.draftYear,
+    highSchool: player.highSchool_school,
+    college: player.college,
+    dYear: player.draft_Year,
+    team: player.team,
+    image: player.image_url,
+    backColor: player.teamColors[0],
+    borderColor: player.teamColors[1],
   };
 }
 
@@ -27,24 +34,33 @@ function injectCard(cardObject) {
   let cardContainer;
   const allCards = document.querySelectorAll(".card-container .card");
   const totalCards = allCards.length;
-  if (totalCards < 4) {
+  if (totalCards < 5) {
     cardContainer = document.getElementById("cardContainer1");
-  } else if (totalCards < 8) {
+  } else if (totalCards < 10) {
     cardContainer = document.getElementById("cardContainer2");
-  } else if (totalCards < 12) {
+  } else if (totalCards < 15) {
     cardContainer = document.getElementById("cardContainer3");
-  } else if (totalCards < 16) {
-    cardContainer = document.getElementById("cardContainer4");
   }
 
   if (cardContainer) {
     cardContainer.insertAdjacentHTML(
       "beforeend",
-      `<div class="card" style="background-color:${cardObject.color};">
-         <h2 class="card-header">${cardObject.text}</h2>
+      `<div class="card" style="border: 4px solid ${
+        cardObject.borderColor
+      }; background-color: ${cardObject.backColor};">
+        h2 class="card-header">${cardObject.name}</h2>
+        <p class="card-year">Year: ${cardObject.year}</p>
+        <p class="card-brand">Brand: ${cardObject.brand}</p>
+        <p class="card-stats">Stats: ${cardObject.stats}</p>
+        <p class="card-bPlace">Birthplace: ${cardObject.bPlace}</p>
+        <p class="card-bYear">Birth Year: ${cardObject.bYear}</p>
+        <p class="card-highSchool">High School: ${cardObject.highSchool}</p>
+        <p class="card-college">College: ${cardObject.college}</p>
+        <p class="card-dYear">Draft Year: ${cardObject.dYear}</p>
+        <p class="card-team">Team: ${cardObject.team}</p>
         ${
-          cardObject.pic
-            ? `<img src="${cardObject.pic}" alt="${cardObject.text}"/>`
+          cardObject.image
+            ? `<img src="${cardObject.image}" alt="${cardObject.text}"/>`
             : ""
         }
       </div>`
@@ -53,16 +69,3 @@ function injectCard(cardObject) {
 
   console.log("Number of cards:", totalCards + 1);
 }
-
-function clearInputFields() {
-  DOMSelectors.nameInput.value = "";
-  DOMSelectors.colorInput.value = "";
-  DOMSelectors.picInput.value = "";
-}
-
-DOMSelectors.submit.addEventListener("click", function (event) {
-  event.preventDefault();
-  const cardObject = createCardObject();
-  injectCard(cardObject);
-  clearInputFields();
-});
